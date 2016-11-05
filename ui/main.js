925b57d8d404521f43148265e78c62b3266de959
renjithrrkj <renjithrrkj@gmail.com>
2016-11-05 19:30:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,10 @@
 console.log('Loaded!');
 //move image
 var img=document.getElementById("madi");
-var marginLeft = 0 ;
 function moveRight(){
-    marginLeft=marginLeft + 5;
+    marginleft = marginleft + 5;
     img.style.marginLeft=marginLeft + 'px';
 }
 img.oneclick = function(){
-    img.style.marginLeft = '100px';
+    img.style.marginLeft = '1000px';
 };
